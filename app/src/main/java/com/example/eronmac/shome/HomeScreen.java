7e5b8f598fda77972a91e31b65e948012f586486
eronmac <eriick23@yahoo.com>
2017-11-08 01:15:42 -0600

    update test commented text

--- app/src/main/java/com/example/eronmac/shome/HomeScreen.java
+++ app/src/main/java/com/example/eronmac/shome/HomeScreen.java
@@ -20,6 +20,8 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
+        // test line
+
         //Link button to layout button
         thermostatbtn=(Button) findViewById(R.id.Thermostatbutton);
         monitor=(Button) findViewById(R.id.Monitorbutton);
