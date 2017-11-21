ff9f28debc2a17dfff1429d4fdec9bf7c83f8be1
eronmac <eriick23@yahoo.com>
2017-11-21 12:58:57 -0600

    made major updates to screen layout <EO>

--- app/src/main/java/com/example/eronmac/shome/HomeScreen.java
+++ app/src/main/java/com/example/eronmac/shome/HomeScreen.java
@@ -1,10 +1,12 @@
 package com.example.eronmac.shome;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
     //create button here
@@ -77,6 +79,13 @@ public class HomeScreen extends AppCompatActivity {
             public void onClick(View view) {
                 Intent x=new Intent(HomeScreen.this,Settings.class);
                 startActivity(x);
+
+                Context context = getApplicationContext();
+                CharSequence text = "Not Implemented";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
 
--- app/src/main/java/com/example/eronmac/shome/Lighting.java
+++ app/src/main/java/com/example/eronmac/shome/Lighting.java
@@ -1,14 +1,73 @@
 package com.example.eronmac.shome;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class Lighting extends AppCompatActivity {
+
+    ToggleButton lightbttn;
+
+    Button addbttn;
+    Button selectbttn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lighting);
+
+
+
+        lightbttn=(ToggleButton) findViewById(R.id.toggleButton6);
+        lightbttn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //Toast message for lightbutton
+                Context context = getApplicationContext();
+                CharSequence text = "Light Turns ON!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        addbttn=(Button) findViewById(R.id.addbutton);
+        addbttn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //Toast message for lightbutton
+                Context context = getApplicationContext();
+                CharSequence text = "Not implemented yet";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+
+        selectbttn=(Button) findViewById(R.id.selectbutton);
+        selectbttn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //Toast message for lightbutton
+                Context context = getApplicationContext();
+                CharSequence text = "Not implemented yet";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
 
     }
 }
